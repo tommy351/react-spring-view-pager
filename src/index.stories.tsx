703fa2a322daea5eba1cf033a645073969aa4dba
Tommy Chen <tommy351@gmail.com>
2019-11-03 15:15:06 +0800

    Setup storybook

---
+++ src/index.stories.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import ViewPager from "./ViewPager";
+
+export default {
+  component: ViewPager,
+  title: "ViewPager"
+};
+
+export function normal() {
+  return <ViewPager />;
+}
